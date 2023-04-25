d3b142d565fe18e2f573d3c0ffd1736c9a5148cf
Hassan Muhh <hassanmuhmdd@gmail.com>
2023-04-25 08:58:36 +0200

    Add components and icons

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "47799e2f55mshbd97c45893cbd9dp13b6dfjsn5f58e83bf98d", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "79cf0fd25b9c6c9d374547e60bd9e3c4"; // enter your key from openweather API
